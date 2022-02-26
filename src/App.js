5c48f519ffee612ca736cd663d7489ace19f4bdb
ivenkateswarlu <44767161+ivenkateswarlu@users.noreply.github.com>
2022-02-26 00:45:33 -0600

    Upated App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-
+import Navbar from './Navbar';
+import Home from './Home';
 function App() {
   const title = 'Welcome to the new blog';
   const likes = 50;
@@ -8,7 +9,10 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
+
       <div className="content">
+        <Home />
         <h1>{ title }</h1>
         <p>Liked { likes } times</p>
 
